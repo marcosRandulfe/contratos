f39f2b679fda38a76b6a8a4c0729648dcfb41db6
marcosrgdev <marcosrandulfegarrido@gmail.com>
2020-11-30 17:23:01 +0100

    [ADD Oficina] ADD Oficina fct

--- index.php
+++ index.php
@@ -1,42 +1,59 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>Contratos de galicia</h1>
-        <?php
-        require './vendor/autoload.php';
+<?php
+        require __DIR__.'/vendor/autoload.php';
 
         use Goutte\Client;
         use Symfony\Component\HttpClient\HttpClient;
         use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
         use Box\Spout\Common\Entity\Row;
-        
         use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 
+        // Variable para contar el número de filas
+        $num_fila_actual=2;
+        function renameExistingFile($filename){
+                $oldname=$filename;
+                $increment = 0;
+                $name=pathinfo($filename, PATHINFO_FILENAME);
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                while(file_exists($filename)) {
+                    $increment++;
+                    $filename = $name. $increment . '.' . $ext;
+                }
+                rename($oldname,$filename);
+        }   
+        
+        function delete_olders($inicio){
+            // 1 mes -> 2595600
+            $ficheros= scandir('.');
+            for ($i=0;$i<count($ficheros);$i++){
+                if (preg_match('/('.$inicio.')\d+.*/', $ficheros[$i])) {
+                     $tiempo=time()-filectime($ficheros[$i]);
+                     if($tiempo> 2595600){
+                         unlink($ficheros[$i]);
+                    }
+                }
+            } 
+        }
+
+        
 
 
-         /**
+        /**
          * 
          * @param type $codigo
          * @return Numero de fila o false
          */
         function comprobarCodigo($codigo){
             $sql ="SELECT fila FROM codigos WHERE codigo='".$codigo."';";
-            $resultado=$GLOBALS['codigos']->query($sql);
-            if($row = $resultado->fetchArray()){
+            $res=$GLOBALS['codigos']->query($sql);
+            if($row= $res->fetchArray()){
                 return $row['fila'];
             }
             return false;
         }
 
         function insertarCodigo($codigo,$fila){
-            $sql="INSERT INTO codigos VALUES(".$codigo.",".$fila.")"; 
-            $resultado=$GLOBALS['codigos']->query($sql);
+            $sql= "INSERT INTO codigos VALUES(".$codigo.",".$fila.");";
+            $GLOBALS['codigos']->query($sql);
         }
 
     // -----------------------------------------------------------------------------------
@@ -50,26 +67,23 @@
             'emailAddress' => $userEmail
         ));
         
-        $request = $service->permissions->create(
+        $service->permissions->create(
             $fileId, $userPermission, array('fields' => 'id')
         );
     }
+
         
-        
-        $inputFileName = './contratosgalicia.ods';
+        $inputFileName = __DIR__.'/contratosgalicia.ods';
+        if(file_exists($inputFileName)){
+            renameExistingFile($inputFileName);
+        }
+        delete_olders(pathinfo($inputFileName,PATHINFO_FILENAME));
         $writer = WriterEntityFactory::createODSWriter();
         $writer->openToFile($inputFileName);
         $GLOBALS['reader']=false;
-     /* 
-      * $reader = ReaderEntityFactory::createODSReader();
-      *  $sheet=$reader->getSheetIterator()->current();
-      */ 
         
-        // Variable para contar el número de filas
-        $num_fila_actual=1;
-        
-        ini_set('max_execution_time', 600); //300 seconds = 5 minutes
-        set_time_limit(600);
+        ini_set('max_execution_time', 60000);
+        set_time_limit(6000);
 
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
@@ -77,60 +91,63 @@
         //Constantes
         define('NOMBRE_FICHERO', './contratosgalicia.ods');
         define('URL_BASE', 'https://www.contratosdegalicia.gal//licitacion?N=');
-        define('NUM_INICIO', 50000);
+        define('NUM_INICIO', 70000);
         //Subir Fichero
-        $client = new Google_Client();
-        // Get your credentials from the console
-        $client->setClientId('1032605733423-mg308q50k5ttk2bcnj5omorv2u6aj95t.apps.googleusercontent.com');
-        $client->setClientSecret('7h7-1DW0g85balewwYRI8x8b');
-        $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
-        $client->setScopes(array('https://www.googleapis.com/auth/drive.file', Google_Service_Gmail::GMAIL_SEND));
+        putenv('GOOGLE_APPLICATION_CREDENTIALS='.__DIR__.'/contratosgalicia.json');
+    
+        $googleClient = new \Google\Client();
+        $googleClient->useApplicationDefaultCredentials();
+        $googleClient->addScope(Google_Service_Drive::DRIVE);
 
-        $codigos = new SQLite3('./codigos.db');
+        $codigos = new SQLite3(__DIR__.'/codigos.db');
+        //resetea los codigos de la base de datos
+        $GLOBALS['codigos']->query("DELETE FROM codigos  WHERE codigo != -1;");
+        //$codigos =  mysqli_connect("localhost", "marcos", "abc123", "codigos") or die;
+        //$codigos = new Flintstone('codigos', ['dir' => __DIR__, 'formatter' => new JsonFormatter()]);
+        //$maximo = new Flintstone('maximo',['dir' => __DIR__, 'formatter' => new JsonFormatter()]);
+       
+        function putMaximo($max){
+            $sql ="INSERT INTO codigos VALUES(-1,".$max.")";
+            $GLOBALS['codigos']->query($sql);
+            $sql = sprintf("UPDATE codigos SET fila=%d WHERE codigo=-1;",$max);
+            $GLOBALS['codigos']->query($sql);
+        }
 
-        if (isset($_GET['code']) || (isset($_SESSION['access_token']) && $_SESSION['access_token'])) {
-        
-        /**
-         * 
-         * @param type $codigo
-         * @return Numero de fila o false
-         */
-        function comprobarCodigo($codigo){
-            $sql ="SELECT fila FROM codigo WHERE codigo='".$codigo."';";
-            $resultado=$GLOBALS['codigos']->query($sql);
-            if($row = $resultado->fetchArray()){
-                return $row['fila'];
+        function getMaximo(){
+            $sql ="SELECT fila FROM codigos WHERE codigo='-1'";
+            $res=$GLOBALS['codigos']->query($sql);
+            if($row = $res->fetchArray()){
+                return $row["fila"];
             }
             return false;
         }
-
+       
+       
         function insertarFila($datos) {
-            $exist_row=false;
             $sheet=$GLOBALS['writer']->getCurrentSheet();
-            echo "<p> Datos: </p>";
-            var_dump($datos);
             $fila = comprobarCodigo($datos[0]);
             if($fila==false){
                 $GLOBALS['writer']->addRow(WriterEntityFactory::createRowFromArray($datos));
+                $GLOBALS['num_fila_actual']++;
+                insertarCodigo($datos[0],$GLOBALS['num_fila_actual']);
             }else{
                 $reader = $GLOBALS['reader'];
                 $sheet=$reader->getSheetIterator()->current();
                 foreach ($sheet->getRowIterator() as $rowIndex => $row) {
                     if($rowIndex==$fila){
-                        $exist_row=true;
                         $cells = WriterEntityFactory::createRowFromArray($datos)->getCells();
                         $row->setCells($cells);
                     }
                 }
             }
         }
-
+        $client= new Client();
         function leerDatosContrato($numero) {
             global $datos_funcion;
             $datos_funcion=[];
             $url = URL_BASE . $numero;
-            echo "<p>jjj" . $url . "</p>";
-            $client = new Client();
+            //echo "<p>url: " . $url . "</p>";
+            $client = $GLOBALS['client'];
             $peticionCorrecta = true;
             try {
                 $crawler = $client->request('GET', $url);
@@ -146,10 +163,8 @@
             $datos_funcion[]=$numero;
 
             $fecha = '';
-            if (!empty($historico = $crawler->filterXPath("//table[@id='tabHistorico']//tr[1]//td[1]"))) {
-                echo "<hr/>";
-                var_dump(count($historico));
-                echo "<hr/>";
+            $historico=$crawler->filterXPath("//table[@id='tabHistorico']//tr[1]//td[1]");
+            if (!empty($historico)) {
                 if (count($historico) > 0) {
                     $fecha = $historico->text();
                 }
@@ -159,22 +174,22 @@
             if (count($dt) > 0) {
                 $dt->each(
                     function ($node) {
-                        $propiedad = $node->text();
                         $valor = "";
                         try {
                             $valor = $node->siblings()->text();
                         } catch (Exception $ex) {
 
                         }
-                        echo "<p>" . $node->text() . "</p>";
-                        echo "<p>" . $valor . "</p>";
+                        //echo "<p>" . $node->text() . "</p>";
+                        //echo "<p>" . $valor . "</p>";
                         $datos_funcion[]=$valor;
                         array_push($GLOBALS['datos_funcion'],$valor);
-                        echo "<h1>Datos cosas</h1>";
-                        var_dump($GLOBALS['datos_funcion']);
+                        //echo "<h1>Datos cosas</h1>";
+                        //var_dump($GLOBALS['datos_funcion']);
                 });
             }
             insertarFila($datos_funcion);
+            return true;
         }
         
         error_reporting(E_ALL);
@@ -183,7 +198,7 @@
         //Se comprueba si la hoja de estilos esta vacia y sino se gurdan los códigos
         $spreadsheet;
        
-        if (file_exists($inputFileName)) {
+        /*if (file_exists($inputFileName)) {
             echo "<p>Existe fichero</p>";
             $reader = ReaderEntityFactory::createReaderFromFile($inputFileName);
             $GLOBALS['reader']=$reader;
@@ -193,28 +208,27 @@
             $inicio = NUM_INICIO;
             $sheet=$reader->getSheetIterator()->current();
             $GLOBALS['sheet_lectura']=$sheet;
+            $num_fila=0;
                 foreach ($sheet->getRowIterator() as $rowIndex => $row) {
                     if ($rowIndex> 1) {
                         $cells = $row->getCells();
                         if(empty($cells)){
                             break;
                         }
-                        $codigo = $cells[0]->getValue();
-                        $filaHoja = $cells[1]->getValue();
-                        $sql = "SELECT codigo, fila FROM codigo WHERE codigo='" . $codigo . "';";
-                        $resultado = $GLOBALS['codigos']->query($sql);
-                        $row = $resultado->fetchArray(SQLITE3_ASSOC);
-                        if (!$row) {
-                            $sql = 'INSERT INTO codigo VALUES (' . $codigo . ',' . $fila . ');';
-                            $GLOBALS['codigos']->query($sql);
-                        }
+                        $codigo = $cells[0];
+                        insertarCodigo($codigo,$rowIndex);                        
                     }
-                    $numFila=$rowIndex;
                 }
-            $GLOBALS['num_fila_actual']=$numFila;
-        }
+           $GLOBALS['num_fila_actual']=$rowIndex;
+        }*/
 
-       if($numFila<=1){
+        // Renombrar el fichero si ya existe
+
+
+
+
+
+       
         $cells = ['id',
                 'Historico(ultima modificacion)',
                 'obxeto',
@@ -247,15 +261,19 @@
             ];
         $row = WriterEntityFactory::createRowFromArray($cells);
         $writer->addRow($row);
-       }
         
         $url = "https://www.contratosdegalicia.gal/rss/ultimas-publicacions.rss";
         $leer_texto = false;
-
-        $fallos = 0;
-        $num_contrato = 700000;
-        while ($fallos < 5000 && $num_contrato<700003) {
-            echo "<p>En el while</p>";
+        $nummaximo=getMaximo();
+        //echo "<h1>Número máximo: $nummaximo</h1>";
+        $fallos = 0; 
+        $num_contrato = 770000;
+        
+        if($nummaximo!=false){
+            $num_contrato=$nummaximo;
+        }
+        $limite= $num_contrato+4000;
+        while ($fallos < 300 && $num_contrato<$limite) {
             $resultado = leerDatosContrato($num_contrato);
             if (!$resultado) {
                 $fallos++;
@@ -264,16 +282,13 @@
             }
             $num_contrato++;
         }
+        putMaximo($num_contrato);
         $writer->close();
-        
        
-            if (isset($_GET['code'])) {
-                $client->fetchAccessTokenWithAuthCode($_GET['code']);
-                $_SESSION['access_token'] = $client->getAccessToken();
-            } else
-                $client->setAccessToken($_SESSION['access_token']);
+       
+        $service = new Google_Service_Drive($googleClient);
 
-            $service = new Google_Service_Drive($client);
+            
 
             //Insert a file
             $file = new Google_Service_Drive_DriveFile();
@@ -281,19 +296,14 @@
             $file->setDescription('Contratos de galicia');
             $file->setMimeType('application/vnd.oasis.opendocument.spreadsheet');
             $fileId=$file->getId();
-            $data = file_get_contents(NOMBRE_FICHERO);
+            $data = file_get_contents($inputFileName);
 
             $createdFile = $service->files->create($file, array(
                 'data' => $data,
                 'mimeType' => 'application/vnd.oasis.opendocument.spreadsheet',
                 'uploadType' => 'multipart'
-            ));
-            addShared($service,$createdFile->getId(), "marcoss.mrgmrg.rg392@gmail.com", "reader");        
-           
-    } else {
-        $authUrl = $client->createAuthUrl();
-        header('Location: ' . $authUrl);
-        exit();
-    }
+            ));  
+            addShared($service,$createdFile->getId(), "marcosrandulfegarrido@gmail.com", "writer");
+            addShared($service,$createdFile->getId(), "jaime.barreiro.laredo@gmail.com", "writer");
 
-        
+exit();
---
+++ indexAntiguo.php
@@ -0,0 +1,212 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contratos de galicia</h1>
+    <?php
+    require './vendor/autoload.php';
+    use Goutte\Client;
+    use Symfony\Component\HttpClient\HttpClient;
+    use PhpOffice\PhpSpreadsheet\Reader\Ods;
+    use PhpOffice\PhpSpreadsheet\Writer\Ods as Writter;
+    use PhpOffice\PhpSpreadsheet\Spreadsheet;
+    use Cache\Adapter\Apcu\ApcuCachePool;
+
+    $pool = new ApcuCachePool();
+    $simpleCache = new \Cache\Bridge\SimpleCache\SimpleCacheBridge($pool);
+    \PhpOffice\PhpSpreadsheet\Settings::setCache($simpleCache);
+
+    ini_set('max_execution_time', 600); //300 seconds = 5 minutes
+    set_time_limit(600);
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+    //Constantes
+    define('NOMBRE_FICHERO','./contratosgalicia.ods');
+    define('URL_BASE','https://www.contratosdegalicia.gal//licitacion?N=');
+    define('NUM_INICIO', 50000);
+    //Subir Fichero
+    //$client = new Google_Client();
+    // Get your credentials from the console
+    //$client->setClientId('1032605733423-mg308q50k5ttk2bcnj5omorv2u6aj95t.apps.googleusercontent.com');
+    //$client->setClientSecret('7h7-1DW0g85balewwYRI8x8b');
+    //$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+    //$client->setScopes(array('https://www.googleapis.com/auth/drive.file'));
+   /// $codigos = new \Ds\Map();
+    
+    $codigos = new SQLite3('./codigos.db');
+
+   // if (isset($_GET['code']) || (isset($_SESSION['access_token']) && $_SESSION['access_token'])) {
+        //$codigos = new \Ds\Map();
+        $id = '';
+        $letra = 'B';
+        $numero = 2;
+        $indice = '';
+
+        function leerDatosContrato($numero){
+                $GLOBALS['letra'] = 'B';
+                $url = URL_BASE.$numero;
+                echo "<p>jjj" . $url . "</p>";
+                $client = new Client();
+                $peticionCorrecta = true;
+                try{
+                    $crawler = $client->request('GET', $url);
+                    if($crawler==null){
+                        $peticionCorrecta=false;
+                    }
+                }catch(Exception $ex){
+                    $peticionCorrecta=false;
+                }
+                if (!$peticionCorrecta) {
+                    return $peticionCorrecta;
+                }
+                $GLOBALS['sheet']->setCellValue('A' . $GLOBALS['numero'], $numero);
+
+                //Calculo del historico
+                $fecha = '';
+                if (!empty($historico = $crawler->filterXPath("//table[@id='tabHistorico']//tr[1]//td[1]"))){
+                    echo "<hr/>";
+                    var_dump(count($historico));
+                    echo "<hr/>";
+                    if(count($historico)>0){
+                        $fecha = $historico->text();
+                    }
+                }
+                $GLOBALS['sheet']->setCellValue('AG' . $GLOBALS['numero'], $fecha);
+
+                $dt =$crawler->filter("dt");
+                if(count($dt)>0){
+                    $dt->each(function ($node) {
+                        $propiedad = $node->text();
+                        $valor= "";
+                        try{
+                            $valor = $node->siblings()->text();
+                        }catch(Exception $ex){
+
+                        }
+                        echo "<p>" . $node->text() . "</p>";
+                        echo "<p>" . var_dump($valor) . "</p>";
+                        echo "<p>Fila y columna: " . $GLOBALS['letra'] . $GLOBALS['numero'] . "</p>";
+                        $GLOBALS['sheet']->setCellValue($GLOBALS['letra'] . $GLOBALS['numero'], $valor);
+                        if($GLOBALS['numero']<3){
+                            $GLOBALS['sheet']->setCellValue($GLOBALS['letra'] . '1', $propiedad);
+                        }
+                        $GLOBALS['letra'] = ++$GLOBALS['letra'];
+                    });
+                }
+                $GLOBALS['numero']++;
+        }
+
+        error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+
+        //Se comprueba si la hoja de estilos esta vacia y sino se gurdan los códigos
+        $spreadsheet;
+        $inputFileName = './contratosgalicia.ods';
+        if(file_exists($inputFileName)){
+            echo "<p>Existe fichero</p>";
+            $reader = new Ods();
+            //$spreadsheet = IOhoja::load('./odsPrueba.ods');
+            $inputFileName = './contratosgalicia.ods';
+            $spreadsheet = $reader->load($inputFileName);
+        }else{
+            $spreadsheet = new Spreadsheet();
+        }
+        $sheet = $spreadsheet->getActiveSheet();
+        $fila = 2;
+        $inicio = NUM_INICIO;
+        if (!is_null($sheet->getCell('A1')) && !is_null($sheet->getCell('A2'))) {
+            $codigo = $sheet->getCell('A' . $fila);
+            //hacer mapa
+            $sql="SELECT codigo, fila FROM codigo WHERE codigo='".$codigo."';";
+            $resultado = $GLOBALS['codigos']->query($sql);
+            $row = $resultado->fetchArray(SQLITE3_ASSOC);
+            if ($row) {
+                $cod_fila = $row['fila'];
+                $sheet->removeRow($cod_fila);
+                $fila--;
+            } else {
+                /* @var \Ds\Map() $codigos */
+                $sql = 'INSERT INTO codigo VALUES ('.$codigo.','.$fila.');';
+                $GLOBALS['codigos']->query($sql);
+            }
+            /*
+             * echo "<p> Claves:".min($codigos->keys()->toArray())."</p>";
+             * $inicio = min($codigos->keys()->toArray());
+            */
+            $sql = 'SELECT min(codigo) FROM codigo;';
+            $resultado=$GLOBALS['codigos']->query($sql);
+            $row = $resultado->fetchArray(SQLITE3_ASSOC);
+        
+            if($row){
+                $fila=$row['codigo'];
+            }else{
+                $fila=0;
+            }
+        }
+        $GLOBALS['numero'] = $fila;
+
+         //Colocación de los nombres de las columnas independientes
+        $GLOBALS['sheet']->setCellValue('AG' . 0, 'Fecha última modificación');
+        $sheet->setCellValue('A1', 'id');
+        $sheet->setCellValue('B1', 'último cambio');
+        $url = "https://www.contratosdegalicia.gal/rss/ultimas-publicacions.rss";
+        $file = $url;
+        $leer_texto = false;
+        $is_item = false;
+        
+        $letra = 'B';
+        $numero = 1;
+        $indice = '';
+
+        $fallos= 0;
+        $num_inicio=500000;
+        while($fallos < 5000){
+            echo "<p>En el while</p>";
+            $resultado=leerDatosContrato($num_inicio);
+            if (!$resultado) {
+                $fallos++;
+            }else{
+                $spreadsheet->garbageCollect();
+            }
+            $num_inicio++;
+        }
+        $writter = new Writter($spreadsheet);
+        $writter->save(NOMBRE_FICHERO);
+/*
+            if (isset($_GET['code'])) {
+                $client->fetchAccessTokenWithAuthCode($_GET['code']);
+                $_SESSION['access_token'] = $client->getAccessToken();
+            } else
+                $client->setAccessToken($_SESSION['access_token']);
+
+            $service = new Google_Service_Drive($client);
+
+            //Insert a file
+            $file = new Google_Service_Drive_DriveFile();
+            $file->setName('fichero.ods');
+            $file->setDescription('Contratos de galicia');
+            $file->setMimeType('application/vnd.oasis.opendocument.spreadsheet');
+
+            $data = file_get_contents(NOMBRE_FICHERO);
+
+            $createdFile = $service->files->create($file, array(
+                'data' => $data,
+                'mimeType' => 'application/vnd.oasis.opendocument.spreadsheet',
+                'uploadType' => 'multipart'
+            ));
+
+            print_r($createdFile);
+*/
+/*    } else {
+        $authUrl = $client->createAuthUrl();
+        header('Location: ' . $authUrl);
+        exit();
+    }
+*/
